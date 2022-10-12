cf914eb4d0ff40f9d2fc94e0aa4ce310c637593c
SimoneLollini <lollinisimone@gmail.com>
2022-10-12 12:13:30 +0200

    fix variables name

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,18 +1,18 @@
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
-const parola1 = prompt("Inserisci una parola")
-const parola2 = prompt("Inserisci un' altra parola")
-let parola;
-if (parola1.length > parola2.length) {
-    console.log("La parola " + parola1 + " è la più lunga");
-    parola = "La parola " + parola1 + " è la più lunga"
-} else if (parola2.length > parola1.length) {
-    console.log("La parola " + parola2 + " è la più lunga");
-    parola = "La parola " + parola2 + " è la più lunga"
+const word1 = prompt("Inserisci una parola")
+const word2 = prompt("Inserisci un' altra parola")
+let word;
+if (word1.length > word2.length) {
+    console.log("La parola " + word1 + " è la più lunga");
+    word = "La parola " + word1 + " è la più lunga"
+} else if (word2.length > word1.length) {
+    console.log("La parola " + word2 + " è la più lunga");
+    word = "La parola " + word2 + " è la più lunga"
 } else {
     console.log("Le due parole sono lunghe uguale");
-    parola = "Le due parole sono lunghe uguale"
+    word = "Le due parole sono lunghe uguale"
 }
 
 document.querySelector('body').innerHTML = parola;
