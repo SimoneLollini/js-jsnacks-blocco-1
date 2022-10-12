8608f5ef4512e1cb57006db53208a0e10e6fcd1f
SimoneLollini <lollinisimone@gmail.com>
2022-10-12 11:48:22 +0200

    add in page

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -5,13 +5,17 @@ const n1 = prompt('Inserisci un numero')
 const n2 = prompt('Inserisci un altro numero')
 
 console.log(n1, n2);
-
+let result;
 if (n1 > n2) {
     console.log("Il primo numero è più grande del secondo!");
+    result = "Il primo numero è più grande del secondo!"
 
 } else if (n2 > n1) {
     console.log("Il secondo numero è più grande del primo!");
+    result = "Il secondo numero è più grande del primo!"
 
 } else {
     console.log("I due numeri sono uguali!");
+    result = "I due numeri sono uguali!"
 }
+document.getElementById('number').innerHTML = result;
