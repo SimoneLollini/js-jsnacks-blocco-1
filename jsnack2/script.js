6771ab1cbe68aee41ce04f91a31ae463fb367ce2
SimoneLollini <lollinisimone@gmail.com>
2022-10-12 12:12:00 +0200

    add output dom

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -3,12 +3,16 @@
 
 const parola1 = prompt("Inserisci una parola")
 const parola2 = prompt("Inserisci un' altra parola")
-
+let parola;
 if (parola1.length > parola2.length) {
     console.log("La parola " + parola1 + " è la più lunga");
+    parola = "La parola " + parola1 + " è la più lunga"
 } else if (parola2.length > parola1.length) {
     console.log("La parola " + parola2 + " è la più lunga");
-
+    parola = "La parola " + parola2 + " è la più lunga"
 } else {
     console.log("Le due parole sono lunghe uguale");
+    parola = "Le due parole sono lunghe uguale"
 }
+
+document.querySelector('body').innerHTML = parola;
