088ca9408a44d539986032978f536c397f2e5d22
SimoneLollini <lollinisimone@gmail.com>
2022-10-12 11:52:27 +0200

    fix output

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,14 +4,14 @@
 const n1 = prompt('Inserisci un numero')
 const n2 = prompt('Inserisci un altro numero')
 
-console.log(n1, n2);
+// console.log(n1, n2);
 let result;
 if (n1 > n2) {
-    console.log("Il primo numero è più grande del secondo!");
+    console.log(n1);
     result = n1
 
 } else if (n2 > n1) {
-    console.log("Il secondo numero è più grande del primo!");
+    console.log(n2);
     result = n2
 
 } else {
