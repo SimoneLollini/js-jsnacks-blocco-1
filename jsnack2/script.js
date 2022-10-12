004ac3a6fa22f13ea4b30440a5f977494bf8df39
SimoneLollini <lollinisimone@gmail.com>
2022-10-12 12:07:38 +0200

    end snack 2 output in console

---
+++ jsnack2/script.js
@@ -0,0 +1,14 @@
+// L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+const parola1 = prompt("Inserisci una parola")
+const parola2 = prompt("Inserisci un' altra parola")
+
+if (parola1.length > parola2.length) {
+    console.log("La parola " + parola1 + " è la più lunga");
+} else if (parola2.length > parola1.length) {
+    console.log("La parola " + parola2 + " è la più lunga");
+
+} else {
+    console.log("Le due parole sono lunghe uguale");
+}
