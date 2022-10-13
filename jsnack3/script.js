feb5e3ffd7fb9a55f7c4baa33b847b4d11296d91
SimoneLollini <lollinisimone@gmail.com>
2022-10-13 12:16:44 +0200

    end sum using while

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,5 +1,8 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
+
+
+/*
 let userNumber;
 let result;
 for (let i = 0; i < 10; i++) {
@@ -10,3 +13,19 @@ for (let i = 0; i < 10; i++) {
     result = valore[0] + [1] + [2] + [3] + [4] + [5] + [6] + [7] + [8] + [9];
     console.log(result);
 }
+*/
+
+// Snack 3 blocco 1 con While:
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+let i = 0
+let sumN = i;
+while (i < 10) {
+
+    console.log(i);
+    const userNumber = Number(prompt("Inserisci un numero"))
+    sumN += userNumber
+
+
+    i++
+} console.log(sumN);
